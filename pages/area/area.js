a8fbda56198d2a44a20cd246a350400675744e62
YashRawat21 <rawat17yash@gmail.com>
2022-10-04 13:07:21 +0530

    updated

--- pages/area/area.js
+++ pages/area/area.js
@@ -3,14 +3,23 @@ const base = document.querySelector('#base-value')
 const height = document.querySelector('#height-value')
 const outputElement = document.querySelector('#output-message')
 
+
+
 calculateButton.addEventListener('click',calculateArea)
 
 function calculateArea() {
 
   const baseLength = base.value 
-  const heightLength = height.value 
-  const product = baseLength * heightLength
-  const area = 1/2 * product
-  outputElement.innerText = "The area is " + area + "cm²"
+  const heightLength = height.value
+
+  if( baseLength < 0 || heightLength < 0 ){
+    outputElement.innerHTML = 'Only positive lengths are accepted !!'
+  } else {
+
+    const product = baseLength * heightLength
+    const area = 1/2 * product
+    outputElement.innerText = "The area is " + area + "cm²"
+  }
+
   
 }
